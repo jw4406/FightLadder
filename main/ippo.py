a1c93b32578d9b4bbe5bddc8a7b35d6348b47f88
jw4406 <jw4406@princeton.edu>
2025-03-07 12:07:07 -0500

    freeze and warmstart done

--- main/ippo.py
+++ main/ippo.py
@@ -175,7 +175,7 @@ def main():
     parser = argparse.ArgumentParser(description='Reset game stats')
     parser.add_argument('--reset', choices=['round', 'match', 'game'], help='Reset stats for a round, a match, or the whole game', default='round')
     parser.add_argument('--model-file', help='The model to continue to learn from')
-    parser.add_argument('--save-dir', help='The directory to save the trained models', default="trained_models/tss_baseline_64_32")
+    parser.add_argument('--save-dir', help='The directory to save the trained models', default="trained_models/magics_ws_tss_4")
     parser.add_argument('--log-dir', help='The directory to save logs', default="logs")
     parser.add_argument('--model-name-prefix', help='The prefix of the model names to save', default="ppo_ryu")
     parser.add_argument('--state', help='The state file to load. By default Champion.Level1.RyuVsGuile', default=SF_DEFAULT_STATE)
@@ -287,12 +287,16 @@ def main():
         print("load model from " + args.left_model_file + " and " + args.right_model_file)
         model.set_parameters_2p(args.left_model_file, args.right_model_file)
     #model.save(os.path.join(args.save_dir, args.model_name_prefix + f"_0_steps"))
-    tss = TSS_PPO.load('/u/jw4406/FightLadder/main/trained_models/ppo_ryu_final_steps.zip', env=env_generator())
-    tss.warmstarted_cont_MAGICS = False
-    model = tss
+    tss = TSS_PPO.load('/home/jw4406/codebase/FightLadder/main/trained_models/ppo_ryu_final_steps.zip', env=env_generator())
+    tss.warmstarted_cont_MAGICS = True
+    tss.dstb_ent_coef = 0
+    tss.ent_coef = 0
     c_learning_rate = 1e-5
     tau_d_v = 5
     tau_c_d = 2
+    tss.warmstart_setup([const_schedule(c_learning_rate * tau_c_d * tau_d_v), const_schedule(c_learning_rate),
+                         const_schedule(c_learning_rate * tau_c_d)])
+    model = tss
     model.v_learning_rate = const_schedule(c_learning_rate * tau_c_d * tau_d_v)
     model.c_learning_rate = const_schedule(c_learning_rate)
     model.d_learning_rate = const_schedule(c_learning_rate * tau_c_d)
