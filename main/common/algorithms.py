b1c197e9abb2dc254d354bd966f9281fe816daea
jw4406 <jw4406@princeton.edu>
2025-03-11 22:19:06 -0400

    just in case

--- main/common/algorithms.py
+++ main/common/algorithms.py
@@ -2454,7 +2454,7 @@ class TSS_PPO(MAGICS_PPO):
             self.logger.record("train/clip_range_vf", clip_range_vf)
         #self.update_ctrl = not self.update_ctrl
 
-class Specialized_Agent(OnPolicyAlgorithm):
+class Specialized_Agent(TSS_PPO):
     policy_aliases: Dict[str, Type[BasePolicy]] = {
         "MlpPolicy": ActorCriticPolicy,
         "CnnPolicy": ActorCriticCnnPolicy,
@@ -2503,7 +2503,12 @@ class Specialized_Agent(OnPolicyAlgorithm):
         super().__init__(
             policy,
             env,
-            learning_rate=v_learning_rate,
+            v_learning_rate=v_learning_rate,
+            c_learning_rate=c_learning_rate,
+            d_learning_rate=d_learning_rate,
+            v_learning_rate_decay=v_learning_rate_decay,
+            c_learning_rate_decay=c_learning_rate_decay,
+            d_learning_rate_decay=d_learning_rate_decay,
             n_steps=n_steps,
             gamma=gamma,
             gae_lambda=gae_lambda,
@@ -2518,19 +2523,14 @@ class Specialized_Agent(OnPolicyAlgorithm):
             device=device,
             seed=seed,
             _init_setup_model=False,
-            supported_action_spaces=(
-                spaces.Box,
-                spaces.Discrete,
-                spaces.MultiDiscrete,
-                spaces.MultiBinary,
-            ),
+            batch_size=batch_size
         )
         self.update_left = I_AM_LEFT
         self.dstb_ent_coef = dstb_ent_coef
         self.dstb_action_space = dstb_action_space
         self.update_right = I_AM_RIGHT
-        self.learning_rate = [v_learning_rate, c_learning_rate, d_learning_rate]
-        self.learning_rate_decay_phase = [v_learning_rate_decay, c_learning_rate_decay, d_learning_rate_decay]
+        #self.learning_rate = [v_learning_rate, c_learning_rate, d_learning_rate]
+        #self.learning_rate_decay_phase = [v_learning_rate_decay, c_learning_rate_decay, d_learning_rate_decay]
         # Sanity check, otherwise it will lead to noisy gradient and NaN
         # because of the advantage normalization
         if normalize_advantage:
@@ -2556,6 +2556,7 @@ class Specialized_Agent(OnPolicyAlgorithm):
                     f"We recommend using a `batch_size` that is a factor of `n_steps * n_envs`.\n"
                     f"Info: (n_steps={self.n_steps} and n_envs={self.env.num_envs})"
                 )
+        '''
         self.batch_size = batch_size
         self.n_epochs = n_epochs
         self.clip_range = clip_range
@@ -2564,6 +2565,7 @@ class Specialized_Agent(OnPolicyAlgorithm):
         self.target_kl = target_kl
         self.smart = True
         self.adversarial = True
+        '''
         self.n_global_env = n_global_env
         self.n_env_per_adv = n_env_per_adv
         if _init_setup_model:
@@ -2741,7 +2743,8 @@ class Specialized_Agent(OnPolicyAlgorithm):
         with th.no_grad():
             # Compute value for the last timestep
             values = self.policy.predict_values(obs_as_tensor(new_obs, self.device))
-
+            not bootstrapped correctly
+            use adversary critics not ma critic
         rollout_buffer.compute_returns_and_advantage(last_values=values, dones=dones)
 
         for i in range(self.num_adversaries):
@@ -2753,4 +2756,158 @@ class Specialized_Agent(OnPolicyAlgorithm):
 
     def train(self):
         # train the special agent and the adversaries
+
+        # main agent needs its own training routine.
+        # adversaries can just call their own methods
+
+        # main agent
+        # need to query adversary critics
+
+        # adversaries
+        for i in range(self.num_adversaries):
+            self.adversaries[i].train()
+
         raise NotImplementedError
+
+    def train_main_agent(self):
+        # helper function
+
+        """
+        Update policy using the currently gathered rollout buffer.
+        """
+        if self.warmstarted_cont_MAGICS is True:
+            if self.warmstarted_cont_MAGICS is True:
+                print("this model is warmstarted! now running magics_ppo training", flush=True)
+            return super().train()
+
+        self._update_learning_rate(
+            [self.policy.ctrl_optimizer, self.policy.dstb_optimizer, self.policy.value_optimizer])
+        # Compute current clip range
+        clip_range = self.clip_range(self._current_progress_remaining)
+        # Optional: clip range for the value function
+        if self.clip_range_vf is not None:
+            clip_range_vf = self.clip_range_vf(self._current_progress_remaining)
+
+        entropy_losses = []
+        pg_losses, value_losses = [], []
+        clip_fractions = []
+
+        continue_training = True
+
+        # train for n_epochs epochs
+        for epoch in range(self.n_epochs):
+            approx_kl_divs = []
+            # Do a complete pass on the rollout buffer
+            for rollout_data in self.rollout_buffer.get(self.batch_size):
+                actions = torch.Tensor(rollout_data.actions).to(self.device)
+                dstb_actions = torch.Tensor(rollout_data.dstb_actions).to(self.device)
+                if isinstance(self.action_space, spaces.Discrete):
+                    # Convert discrete action from float to long
+                    actions = rollout_data.actions.long().flatten()
+
+                # Re-sample the noise matrix because the log_std has changed
+                if self.use_sde:
+                    self.policy.reset_noise(self.batch_size)
+
+                values, ctrl_log_prob, ctrl_entropy, dstb_log_prob, dstb_entropy = self.policy.evaluate_actions(
+                    torch.Tensor(rollout_data.observations).to(self.device), actions, dstb_actions)
+                values = values.flatten()
+                # Normalize advantage
+                advantages = torch.from_numpy(rollout_data.advantages).to(self.device)
+                # Normalization does not make sense if mini batchsize == 1, see GH issue #325
+                if self.normalize_advantage and len(advantages) > 1:
+                    advantages = (advantages - advantages.mean()) / (advantages.std() + 1e-8)
+
+                # ratio between old and new policy, should be one at the first iteration
+                ctrl_ratio = th.exp(ctrl_log_prob - torch.Tensor(rollout_data.old_log_prob).to(self.device))
+                dstb_ratio = th.exp(dstb_log_prob - torch.Tensor(rollout_data.old_dstb_log_prob).to(self.device))
+
+                # clipped surrogate loss
+                policy_loss_1 = advantages * ctrl_ratio
+                policy_loss_2 = advantages * th.clamp(ctrl_ratio, 1 - clip_range, 1 + clip_range)
+                dstb_policy_loss_1 = advantages * dstb_ratio
+                dstb_policy_loss_2 = advantages * th.clamp(dstb_ratio, 1 - clip_range, 1 + clip_range)
+                ctrl_policy_loss = -th.min(policy_loss_1, policy_loss_2).mean()
+                dstb_policy_loss = th.min(dstb_policy_loss_1, dstb_policy_loss_2).mean()
+
+                # Logging
+                pg_losses.append(ctrl_policy_loss.item())
+                clip_fraction = th.mean((th.abs(ctrl_ratio - 1) > clip_range).float()).item()
+                clip_fractions.append(clip_fraction)
+
+                if self.clip_range_vf is None:
+                    # No clipping
+                    values_pred = values
+                else:
+                    # Clip the difference between old and new value
+                    # NOTE: this depends on the reward scaling
+                    values_pred = rollout_data.old_values + th.clamp(
+                        values - rollout_data.old_values, -clip_range_vf, clip_range_vf
+                    )
+                # Value loss using the TD(gae_lambda) target
+                value_loss = F.mse_loss(torch.Tensor(rollout_data.returns).to(self.device), values_pred)
+                value_losses.append(value_loss.item())
+
+                # Entropy loss favor exploration
+                if (ctrl_entropy is None) or (dstb_entropy is None):
+                    # Approximate entropy when no analytical form
+                    ctrl_entropy_loss = -th.mean(-ctrl_log_prob)
+                    dstb_entropy_loss = -th.mean(-dstb_log_prob)
+                else:
+                    ctrl_entropy_loss = -th.mean(ctrl_entropy)
+                    dstb_entropy_loss = -th.mean(dstb_entropy)
+
+                entropy_losses.append(ctrl_entropy_loss.item())
+
+                loss = ctrl_policy_loss + self.ent_coef * ctrl_entropy_loss + self.dstb_ent_coef * dstb_entropy_loss + self.vf_coef * value_loss + dstb_policy_loss
+
+                # Calculate approximate form of reverse KL Divergence for early stopping
+                # see issue #417: https://github.com/DLR-RM/stable-baselines3/issues/417
+                # and discussion in PR #419: https://github.com/DLR-RM/stable-baselines3/pull/419
+                # and Schulman blog: http://joschu.net/blog/kl-approx.html
+                with th.no_grad():
+                    ctrl_log_ratio = ctrl_log_prob - torch.from_numpy(rollout_data.old_log_prob).to(self.device)
+                    ctrl_approx_kl_div = th.mean((th.exp(ctrl_log_ratio) - 1) - ctrl_log_ratio).cpu().numpy()
+                    dstb_log_ratio = dstb_log_prob - torch.from_numpy(rollout_data.old_dstb_log_prob).to(self.device)
+                    dstb_approx_kl_div = th.mean((th.exp(dstb_log_ratio) - 1) - dstb_log_ratio).cpu().numpy()
+                    approx_kl_divs.append(ctrl_approx_kl_div)
+
+                if self.target_kl is not None and torch.max(ctrl_approx_kl_div,
+                                                            dstb_approx_kl_div) > 1.5 * self.target_kl:
+                    continue_training = False
+                    if self.verbose >= 1:
+                        print(f"Early stopping at step {epoch} due to reaching max kl: {approx_kl_div:.2f}")
+                    break
+
+                # Optimization step
+                self.policy.ctrl_optimizer.zero_grad()
+                self.policy.dstb_optimizer.zero_grad()
+                self.policy.value_optimizer.zero_grad()
+                loss.backward()
+                # Clip grad norm
+                th.nn.utils.clip_grad_norm_(self.policy.parameters(), self.max_grad_norm)
+                self.policy.ctrl_optimizer.step()
+                self.policy.dstb_optimizer.step()
+                self.policy.value_optimizer.step()
+
+            if not continue_training:
+                break
+
+        self._n_updates += self.n_epochs
+        explained_var = explained_variance(self.rollout_buffer.values.flatten(), self.rollout_buffer.returns.flatten())
+
+        # Logs
+        self.logger.record(f"train/entropy_loss", np.mean(entropy_losses))
+        self.logger.record(f"train/policy_gradient_loss", np.mean(pg_losses))
+        self.logger.record(f"train/value_loss", np.mean(value_losses))
+        self.logger.record(f"train/approx_kl", np.mean(approx_kl_divs))
+        self.logger.record(f"train/clip_fraction", np.mean(clip_fractions))
+        self.logger.record(f"train/loss", loss.item())
+        self.logger.record(f"train/explained_variance", explained_var)
+        if hasattr(self.policy, "log_std"):
+            self.logger.record(f"train/std", th.exp(self.policy.log_std).mean().item())
+
+        self.logger.record("train/n_updates", self._n_updates, exclude="tensorboard")
+        self.logger.record("train/clip_range", clip_range)
+        if self.clip_range_vf is not None:
+            self.logger.record("train/clip_range_vf", clip_range_vf)
