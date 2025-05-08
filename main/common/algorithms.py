9d9e5a52c5695870d7b29ce3d5eeff094cc09042
Justin S. Wang <jw4406@princeton.edu>
2025-05-07 21:12:16 -0400

    exploiter class

--- main/common/algorithms.py
+++ main/common/algorithms.py
@@ -4793,3 +4793,161 @@ class eepy(MAGICS_PPO):
         self.logger.record("train/clip_range", clip_range)
         if self.clip_range_vf is not None:
             self.logger.record("train/clip_range_vf", clip_range_vf)
+
+class Exploiter(PPO):
+    def __init__(self,
+        policy: Union[str, Type[ActorCriticPolicy]],
+        env: Union[GymEnv, str],
+        learning_rate: Union[float, Schedule] = 3e-4,
+        n_steps: int = 2048,
+        batch_size: int = 64,
+        n_epochs: int = 10,
+        gamma: float = 0.99,
+        gae_lambda: float = 0.95,
+        clip_range: Union[float, Schedule] = 0.2,
+        clip_range_vf: Union[None, float, Schedule] = None,
+        normalize_advantage: bool = True,
+        ent_coef: float = 0.0,
+        vf_coef: float = 0.5,
+        max_grad_norm: float = 0.5,
+        use_sde: bool = False,
+        sde_sample_freq: int = -1,
+        target_kl: Optional[float] = None,
+        tensorboard_log: Optional[str] = None,
+        policy_kwargs: Optional[Dict[str, Any]] = None,
+        verbose: int = 0,
+        seed: Optional[int] = None,
+        device: Union[th.device, str] = "auto",
+        _init_setup_model: bool = True,
+        exploited: Union[Specialized_Agent, Specialized_Agent_IPPO]=None
+    ):
+
+        super().__init__(policy=policy,
+        env=env,
+        learning_rate=learning_rate,
+        n_steps=n_steps,
+        batch_size=batch_size,
+        n_epochs=n_epochs,
+        gamma=gamma,
+        gae_lambda=gae_lambda,
+        clip_range=clip_range,
+        clip_range_vf=clip_range_vf,
+        normalize_advantage=normalize_advantage,
+        ent_coef=ent_coef,
+        vf_coef=vf_coef,
+        max_grad_norm=max_grad_norm,
+        use_sde=use_sde,
+        sde_sample_freq=sde_sample_freq,
+        target_kl=target_kl,
+        tensorboard_log=tensorboard_log,
+        policy_kwargs=policy_kwargs,
+        verbose=verbose,
+        seed=seed,
+        device=device,
+        _init_setup_model=_init_setup_model)
+
+        assert exploited is not None
+
+        self.exploited = exploited
+
+    def collect_rollouts(
+        self,
+        env: VecEnv,
+        callback: BaseCallback,
+        rollout_buffer: RolloutBuffer,
+        n_rollout_steps: int,
+    ) -> bool:
+
+        """
+                Collect experiences using the current policy and fill a ``RolloutBuffer``.
+                The term rollout here refers to the model-free notion and should not
+                be used with the concept of rollout used in model-based RL or planning.
+
+                :param env: The training environment
+                :param callback: Callback that will be called at each step
+                    (and at the beginning and end of the rollout)
+                :param rollout_buffer: Buffer to fill with rollouts
+                :param n_rollout_steps: Number of experiences to collect per environment
+                :return: True if function returned with at least `n_rollout_steps`
+                    collected, False if callback terminated rollout prematurely.
+                """
+        assert self._last_obs is not None, "No previous observation was provided"
+        # Switch to eval mode (this affects batch norm / dropout)
+        self.policy.set_training_mode(False)
+
+        n_steps = 0
+        rollout_buffer.reset()
+        # Sample new weights for the state dependent exploration
+        if self.use_sde:
+            self.policy.reset_noise(env.num_envs)
+
+        callback.on_rollout_start()
+
+        while n_steps < n_rollout_steps:
+            if self.use_sde and self.sde_sample_freq > 0 and n_steps % self.sde_sample_freq == 0:
+                # Sample a new noise matrix
+                self.policy.reset_noise(env.num_envs)
+
+            with th.no_grad():
+                # Convert to pytorch tensor or to TensorDict
+                obs_tensor = obs_as_tensor(self._last_obs, self.device)
+                actions, values, log_probs, = self.policy(obs_tensor)
+                EXPLOITED_ACTIONS, _, _, _, _ = self.exploited.policy(obs_tensor)
+            actions = actions.cpu().numpy()
+            EXPLOITED_ACTIONS = EXPLOITED_ACTIONS.cpu().numpy()
+            #dstb_actions = dstb_actions.cpu().numpy()
+            # Rescale and perform action
+            clipped_actions = np.hstack([actions, EXPLOITED_ACTIONS])
+            # Clip the actions to avoid out of bound error
+            if isinstance(self.action_space, spaces.Box):
+                clipped_actions = np.clip(actions, self.action_space.low, self.action_space.high)
+
+            new_obs, rewards, rew_other, dones, infos = env.step(clipped_actions)
+            # assert np.allclose(rewards + rew_other, np.zeros(rewards.shape))
+            self.num_timesteps += env.num_envs
+
+            # Give access to local variables
+            callback.update_locals(locals())
+            if callback.on_step() is False:
+                return False
+
+            self._update_info_buffer(infos)
+            n_steps += 1
+
+            if isinstance(self.action_space, spaces.Discrete):
+                # Reshape in case of discrete action
+                actions = actions.reshape(-1, 1)
+
+            # Handle timeout by bootstraping with value function
+            # see GitHub issue #633
+            for idx, done in enumerate(dones):
+                if (
+                        done
+                        and infos[idx].get("terminal_observation") is not None
+                        and infos[idx].get("TimeLimit.truncated", False)
+                ):
+                    terminal_obs = self.policy.obs_to_tensor(infos[idx]["terminal_observation"])[0]
+                    with th.no_grad():
+                        terminal_value = self.policy.predict_values(terminal_obs)[0]
+                    rewards[idx] += self.gamma * terminal_value
+
+            rollout_buffer.add(
+                self._last_obs,  # type: ignore[arg-type]
+                actions,
+                rewards,
+                self._last_episode_starts,  # type: ignore[arg-type]
+                values,
+                log_probs
+            )
+            self._last_obs = new_obs
+            self._last_episode_starts = dones
+
+        with th.no_grad():
+            # Compute value for the last timestep
+            values = self.policy.predict_values(obs_as_tensor(new_obs, self.device))
+
+        rollout_buffer.compute_returns_and_advantage(last_values=values, dones=dones)
+
+        callback.on_rollout_end()
+
+        return True
