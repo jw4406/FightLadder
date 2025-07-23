41d4383d80088ee344743f4db6d174bd6500b362
Amit Solomon <as3993@princeton.edu>
2025-07-23 15:21:55 -0400

    Updated select_device and move_policy in utils.

--- main/utils.py
+++ main/utils.py
@@ -13,10 +13,40 @@ def agent_win(info: dict) -> bool:
     """
     return info['enemy_hp'] < info['agent_hp']
 
-def select_device() -> str:
+def select_device(device_id: int=0) -> torch.device:
     """
     This function returns "cuda" if it is available and "cpu" otherwise
     """
     if torch.cuda.is_available():
-        return "cuda"
-    return "cpu"
+        return torch.device(f"cuda:{device_id}")
+    return torch.device("cpu")
+
+def move_policy(policy: torch.nn.Module, device: torch.device) -> None:
+    """
+    This function moves a policy to a selected device.
+    """
+    def _move_if_exists(policy: torch.nn.Module, device: torch.device, component_name: str) -> None:
+        """
+        This function moves policy.component to device if it exists.
+        """
+        if not hasattr(policy, component_name):
+            return
+        component = getattr(policy, component_name)
+        if not isinstance(component, list): #Deal only with lists
+            component = [component]
+        for item in component:
+            item.to(device)
+    
+    policy.to(device)
+    for param in policy.parameters():
+        param.data = param.data.to(device)
+        if param.grad is not None:
+            param.grad = param.grad.to(device)
+    for buffer in policy.buffers():
+        buffer.data = buffer.data.to(device)
+    for child in policy.children():
+        move_policy(child, device)
+    _move_if_exists(policy, device, "mlp_extractor")
+    _move_if_exists(policy, device, "features_extractor")
+    _move_if_exists(policy, device, "action_net")
+    _move_if_exists(policy, device, "value_net")
