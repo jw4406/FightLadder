6ae2a9075c2d9df79090636b1fc3107445bcd90c
jw4406 <jw4406@princeton.edu>
2025-08-04 16:07:05 -0400

    move only value forward pass out of torch.no_grad in policies

--- main/common/justin/derivative_free_spar.py
+++ main/common/justin/derivative_free_spar.py
@@ -102,26 +102,41 @@ def _update_single_value_function(batch_size: int, max_grad_norm: float, policy,
     actions_batch, dstb_actions_batch, observations_batch, returns_batch, all_env_indices = _prep_rollout_data_actions(batch_size, buffer)
     policy.num_global_env = num_envs
     policy.num_adv = 1
-    for i in range(len(returns_batch) // batch_size):
-        values, _, _, _, _ = policy.evaluate_actions(
-            observations_batch,
-            actions_batch,
-            dstb_actions_batch,
-            shuffle_keys=all_env_indices,
-            network_keys=[adversary_index]
-            )
-        values = values.flatten()
+    #for i in range(len(returns_batch) // batch_size): # should this loop be here?
+    # values, _, _, _, _ = policy.evaluate_actions(
+    #     observations_batch,
+    #     actions_batch,
+    #     dstb_actions_batch,
+    #     shuffle_keys=all_env_indices,
+    #     network_keys=[adversary_index]
+    #     )
+    #values = values.flatten()
         #test = F.mse_loss(returns_batch, values)
         #value_grads = th.autograd.grad(test, policy.value_optimizer.param_groups[0]['params'][adversary_index], create_graph=True)
         # Single loss calculation and optimization step
         #value_losses = []
-        value_loss = F.mse_loss(returns_batch, values)
-        #value_grads = th.autograd.grad(value_loss, policy.value_optimizer.param_groups[0]['params'], create_graph=True)
+    for i in range(len(returns_batch) // batch_size):
+        values, _, _, _, _ = policy.evaluate_actions(
+        observations_batch[i * batch_size:(i + 1) * batch_size],
+        actions_batch[i * batch_size:(i + 1) * batch_size],
+        dstb_actions_batch[i * batch_size:(i + 1) * batch_size],
+        shuffle_keys=all_env_indices[i * batch_size:(i + 1) * batch_size],
+        network_keys=[adversary_index]
+        )
+        values = values.flatten()
+        offset = 12 # vf extractor and shared trunk are 12
+        num_per_head = 10 # lstm = 6, 2 linear layers = 2 + 2, total 10
+        value_loss = F.mse_loss(returns_batch[i * batch_size:(i + 1) * batch_size], values)
+        indices = list(range(0, offset)) + list(range(offset + adversary_index * num_per_head, offset + (adversary_index + 1) * num_per_head))
+        value_grads = th.autograd.grad(value_loss, [policy.value_optimizer.param_groups[0]['params'][j] for j in indices], create_graph=True)
         #value_grads = th.cat([grad.view(-1) for grad in value_grads])
         policy.value_optimizer.zero_grad()
+        for i in range(len(value_grads)):
+            policy.value_optimizer.param_groups[0]['params'][indices[i]].grad = value_grads[i]
+        #policy.value_optimizer.zero_grad()
         #for i in range(len(policy.value_optimizer.param_groups[0]['params'])):
         #    policy.value_optimizer.param_groups[0]['params'][i].grad = value_grads[i]
-        value_loss.backward()
+        #value_loss.backward()
         th.nn.utils.clip_grad_norm_(policy.parameters(), max_grad_norm)
         policy.value_optimizer.step()
 
