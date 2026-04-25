f32ac6be5aad661b51235428ac8c59deb110aa8c
Justin S. Wang <jw4406@princeton.edu>
2026-04-24 21:20:39 -0400

    add duel in worktree, test continue exploiters

--- run_br_workers.sh
+++ run_br_workers.sh
@@ -14,7 +14,7 @@ BR_WORKER_PATH="${SCRIPT_DIR}/main/new_br_worker.py"
 
 # Arguments from launch.json (Python Debugger: new_br_worker.py)
 EVAL_PROT="True"
-EVAL_ADV="True"
+EVAL_ADV="False"
 EVAL_ONLY="False"
 PROJ_NAME="br_training_ema_test_2"
 ANALYSIS_UPLOAD_PROJ_NAME="br_analysis"
